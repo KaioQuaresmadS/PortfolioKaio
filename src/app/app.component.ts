eecf4689d62933187c39557c3861832e03230310
Kaio Quaresma <kaioquaresmadesouza1@gmail.com>
2025-02-27 12:01:34 -0300

    Criado os botões e feito a parte visual da página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IndexComponent],
+  imports: [IndexComponent],
   template: `
     <app-index />
   `,
--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -10,5 +10,8 @@ import { Component } from '@angular/core';
 export class IndexComponent {
 
   public src = 'https://wallpapers.com/images/high/afternoon-drift-cars-0ijcnmj3hbsogfnp.webp';
+  public srcProfile = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqafzhnwwYzuOTjTlaYMeQ7hxQLy_Wq8dnQg&s';
+  public photoProfile = 'Foto de perfil';
+  public author = 'Kaio Quaresma de Souza';
 
 }
