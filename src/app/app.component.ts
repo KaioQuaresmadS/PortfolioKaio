d013ecd4f05d202c41ef3e1915a505200f10c890
Kaio Quaresma <kaioquaresmadesouza1@gmail.com>
2025-02-26 23:56:15 -0300

    Adicionei a imagem e a estrutura da navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [RouterOutlet, IndexComponent],
+  template: `
+    <app-index />
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolioSite';
+  title = 'Kaio Quaresma de Souza';
 }
---
+++ src/app/components/index/index.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-index',
+  imports: [],
+  templateUrl: './index.component.html',
+  styleUrl: './index.component.css'
+})
+export class IndexComponent {
+
+  public src = 'https://wallpapers.com/images/high/afternoon-drift-cars-0ijcnmj3hbsogfnp.webp';
+
+}
