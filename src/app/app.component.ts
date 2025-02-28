a96a39b64d6b3c12afe4e7b97838e950a52320b4
Kaio Quaresma <kaioquaresmadesouza1@gmail.com>
2025-02-28 20:26:02 -0300

    Adicionado caminho para servidor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -13,4 +15,11 @@ import { IndexComponent } from './components/index/index.component';
 })
 export class AppComponent {
   title = 'Kaio Quaresma de Souza';
+}
+export class ImagemService {
+  constructor(private http: HttpClient) {}
+  
+  getImagem(id: number){
+    return this.http.get(`https://localhost:5000/api/imagens/${id}`)
+  }
 }
